565073856d1ea31a42aaf641ac106ecfb9182fca
strafk3 <andrius578@gmail.com>
2023-01-17 16:35:24 +0200

    feat(1): fixed settings in tsconfig.json, and described typed

--- src/types/car-joined.ts
+++ src/types/car-joined.ts
@@ -1,5 +1,10 @@
-type CarJoined = {
+import Brand from './brand';
+import type Car from './car';
+import Model from './model';
 
+type CarJoined = Omit<Car, 'modelId'> & {
+  brand: Brand['title'],
+  model: Model['title'],
 };
 
 export default CarJoined;
