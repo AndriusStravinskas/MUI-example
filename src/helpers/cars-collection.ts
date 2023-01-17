f4ce268354f2b0ee94eacfe747d31e6f8ecf5006
strafk3 <andrius578@gmail.com>
2023-01-17 18:21:08 +0200

    feat(1): CarsCollection constructor, joinCar and getter all

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,9 +1,36 @@
-type CarsCollectionProps = {
+import type Brand from '../types/brand';
+import type Car from '../types/car';
+import type CarJoined from '../types/car-joined';
+import type Model from '../types/model';
 
+type CarsCollectionProps = {
+  cars: Car[],
+  brands: Brand[],
+  models: Model[],
 };
 
 class CarsCollection {
+  private props: CarsCollectionProps;
 
+  constructor(props: CarsCollectionProps) {
+    this.props = props;
+  }
+
+  public get all():CarJoined[] {
+    return this.props.cars.map(this.joinCar);
+  }
+
+  private joinCar = ({ modelId, ...car }: Car) => {
+    const { brands, models } = this.props;
+    const carModel = models.find((model) => model.id === modelId);
+    const carBrand = brands.find((brand) => brand.id === carModel?.brandId);
+
+    return {
+      ...car,
+      brand: (carBrand && carBrand.title) ?? 'unknown',
+      model: (carModel && carModel.title) ?? 'unknown',
+    };
+  };
 }
 
 export default CarsCollection;
