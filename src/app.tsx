60beebfb252d69d98e3b870cebdf31a8ecb723c2
strafk3 <andrius578@gmail.com>
2023-02-06 19:58:32 +0200

    feat: created button-page and typography-page.tsx files

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import TypographyPage from 'pages/typography-page';
+import ButtonPage from 'pages/button-page';
+import { Box } from '@mui/material';
 
 const App = () => (
-  <TypographyPage />
+  <Box sx={{
+    padding: 4,
+  }}
+  >
+    {/* <TypographyPage /> */}
+    <ButtonPage />
+  </Box>
 );
 
 export default App;
